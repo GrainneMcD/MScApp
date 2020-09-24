d8479e083fe064b51466b10db8a70f78f8c2db71
Grainne McD <grainne_mcdowell@hotmail.com>
2020-09-24 16:06:34 +0100

    initial create and full test coverage

---
+++ MScAppTest/QuestionTestTest.cs
@@ -0,0 +1,51 @@
+﻿using MScApp.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace MScAppTest
+{
+    public class QuestionTestTest
+    {
+        MScApp.Core.QuestionTest questionTest;
+        Question validQuestion;
+        readonly int validQuestionID = 1;
+        Test validTest;
+        readonly int validTestID = 1;
+
+        [Fact]
+        public void QuestionTestConstructorTest()
+        {
+            questionTest = new MScApp.Core.QuestionTest();
+
+            Assert.NotNull(questionTest);
+            Assert.IsType<MScApp.Core.QuestionTest>(questionTest);
+        }
+
+        [Fact]
+        public void QuestionTestPropertiesTest()
+        {
+            validQuestion = new Question() { ID = validQuestionID };
+            validTest = new Test() { ID = validTestID };
+            questionTest = new MScApp.Core.QuestionTest()
+            {
+                Question = validQuestion,
+                QuestionID = validQuestion.ID,
+                Test = validTest,
+                TestID = validTest.ID
+            };
+
+
+            Assert.NotNull(validTest);
+            Assert.IsType<Test>(validTest);
+            Assert.NotNull(validQuestion);
+            Assert.IsType<Question>(validQuestion);
+
+            Assert.Equal(validTest, questionTest.Test);
+            Assert.Equal(validTestID, questionTest.TestID);
+            Assert.Equal(validQuestion, questionTest.Question);
+            Assert.Equal(validQuestionID, questionTest.QuestionID);
+        }
+    }
+}
---
+++ MScAppTest/QuestionTypeTest.cs
@@ -0,0 +1,61 @@
+﻿using MScApp.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace MScAppTest
+{
+    public class QuestionTypeTest
+    {
+        QuestionType questionType;
+        List<Question> questions;
+        readonly string validQuestionTypeName1 = "Written and Verbal Reasoning";
+        readonly QuestionTypeID validQuestionType1 = QuestionTypeID.Written_and_Verbal_Reasoning;
+        readonly string validQuestionTypeName2 = "Diagrammatic Reasoning";
+        readonly QuestionTypeID validQuestionType2 = QuestionTypeID.Diagrammatic_Reasoning;
+        readonly string validQuestionTypeName3 = "Symbolic Manipulation";
+        readonly QuestionTypeID validQuestionType3 = QuestionTypeID.Symbolic_Manipulation;
+
+        [Fact]
+        public void QuestionTypeConstructorTest()
+        {
+            questionType = new QuestionType();
+
+            Assert.NotNull(questionType);
+            Assert.IsType<Question>(questionType);
+        }
+
+        [Fact]
+        public void QuestionTypePropertiesTest()
+        {
+            questions = new List<Question>();
+            questionType = new QuestionType()
+            {
+                Questions = questions,
+                QuestionTypeID = validQuestionType1,
+                QuestionTypeName = validQuestionTypeName1
+            };
+            Assert.NotNull(questionType.Questions);
+            Assert.IsType<List<Question>>(questionType.Questions);
+
+            Assert.Equal(questions, questionType.Questions);
+            Assert.Equal(validQuestionTypeName1, questionType.QuestionTypeName);
+            Assert.Equal(validQuestionType1, questionType.QuestionTypeID);
+
+        }
+
+        [Fact]
+        public void QuestionTypeIDTest()
+        {
+            questionType = new QuestionType() { QuestionTypeID = QuestionTypeID.Written_and_Verbal_Reasoning };
+            Assert.Equal(validQuestionType1, questionType.QuestionTypeID);
+
+            questionType = new QuestionType() { QuestionTypeID = QuestionTypeID.Diagrammatic_Reasoning };
+            Assert.Equal(validQuestionType2, questionType.QuestionTypeID);
+
+            questionType = new QuestionType() { QuestionTypeID = QuestionTypeID.Symbolic_Manipulation };
+            Assert.Equal(validQuestionType3, questionType.QuestionTypeID);
+        }
+    }
+}
